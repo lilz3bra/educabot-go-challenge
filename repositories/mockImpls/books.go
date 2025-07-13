0822fb4ef420aa291ffc2e5ba6817c9a68e4f22c
lilz3bra <21014124+lilz3bra@users.noreply.github.com>
2025-07-13 16:49:59 -0300

    feat: Update GetBooks method to return error alongside book list

--- repositories/mockImpls/books.go
+++ repositories/mockImpls/books.go
@@ -14,10 +14,10 @@ func NewMockBooksProvider() *MockBooksProvider {
 	return &MockBooksProvider{}
 }
 
-func (m *MockBooksProvider) GetBooks(_ context.Context) []models.Book {
+func (m *MockBooksProvider) GetBooks(_ context.Context) ([]models.Book, error) {
 	return []models.Book{
 		{ID: 1, Name: "The Go Programming Language", Author: "Alan Donovan", UnitsSold: 5000, Price: 40},
 		{ID: 2, Name: "Clean Code", Author: "Robert C. Martin", UnitsSold: 15000, Price: 50},
 		{ID: 3, Name: "The Pragmatic Programmer", Author: "Andrew Hunt", UnitsSold: 13000, Price: 45},
-	}
+	}, nil
 }
